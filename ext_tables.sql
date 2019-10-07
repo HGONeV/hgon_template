b37b4be1dd582dbc98a78f13753c374de6e80865
Maximilian Faessler <maximilian@faesslerweb.de>
2019-10-07 14:38:40 +0000

    [DELETE] Moving files to hgon_payment

--- ext_tables.sql
+++ ext_tables.sql
@@ -59,37 +59,6 @@ CREATE TABLE tx_hgontemplate_domain_model_article (
 
 );
 
-
-#
-# Table structure for table 'tx_hgontemplate_domain_model_paymentprofile'
-#
-CREATE TABLE tx_hgontemplate_domain_model_paymentprofile (
-
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-
-	title varchar(255) DEFAULT '' NOT NULL,
-	description text,
-	profile_id varchar(255) DEFAULT '' NOT NULL,
-
-	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
-	crdate int(11) unsigned DEFAULT '0' NOT NULL,
-	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
-	deleted smallint(5) unsigned DEFAULT '0' NOT NULL,
-	hidden smallint(5) unsigned DEFAULT '0' NOT NULL,
-
-	sys_language_uid int(11) DEFAULT '0' NOT NULL,
-	l10n_parent int(11) DEFAULT '0' NOT NULL,
-	l10n_diffsource mediumblob,
-	l10n_state text,
-
-	PRIMARY KEY (uid),
-	KEY parent (pid),
-	KEY language (l10n_parent,sys_language_uid)
-
-);
-
-
 #
 # Table structure for table 'pages'
 #
